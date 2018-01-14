bdfdc5833d734803c4897cf841a7076bb0afd485
liangze <249834096@qq.com>
2018-01-14 12:29:12 +0800

    project init

---
+++ vendor/tcg/voyager/resources/views/partials/app-footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="app-footer">
+    <div class="site-footer-right">
+        @if (rand(1,100) == 100)
+            <i class="voyager-rum-1"></i> {{ __('voyager.theme.footer_copyright2') }}
+        @else
+            {!! __('voyager.theme.footer_copyright') !!} GUO XUN
+        @endif
+        @if (!empty($version))
+            - {{ $version }}
+        @endif
+    </div>
+</footer>
